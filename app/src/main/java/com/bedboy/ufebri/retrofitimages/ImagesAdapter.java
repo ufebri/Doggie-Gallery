e2d3f08ddccaa7042b7981bf92b8b8a04f249fcc
ufebri <ufebri@live.com>
2020-05-08 19:23:22 +0700

    Migration to Android X

--- app/src/main/java/com/bedboy/ufebri/retrofitimages/ImagesAdapter.java
+++ app/src/main/java/com/bedboy/ufebri/retrofitimages/ImagesAdapter.java
@@ -1,15 +1,14 @@
 package com.bedboy.ufebri.retrofitimages;
 
 import android.content.Context;
-import android.media.Image;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.bedboy.ufebri.retrofitimages.Utils.Images;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
--- app/src/main/java/com/bedboy/ufebri/retrofitimages/MainActivity.java
+++ app/src/main/java/com/bedboy/ufebri/retrofitimages/MainActivity.java
@@ -1,10 +1,10 @@
 package com.bedboy.ufebri.retrofitimages;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 
 import com.bedboy.ufebri.retrofitimages.Utils.BaseApps;
@@ -16,6 +16,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
